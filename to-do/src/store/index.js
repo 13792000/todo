9bcc9a727443f592c877077ad87b09d7d3673ab9
hosein <hosein.asgari2014@gmail.com>
2020-12-08 16:12:26 +0330

    change tittle

--- to-do/src/store/index.js
+++ to-do/src/store/index.js
@@ -6,11 +6,11 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     items:[
-      {id: 1, tittle: 'do'},
-      {id: 2, tittle: 'do'},
-      {id: 3, tittle: 'do'},
-      {id: 4, tittle: 'do'},
-      {id: 5, tittle: 'do'}
+      {id: 1, tittle: 'practice bootstrap'},
+      {id: 2, tittle: 'practice vue'},
+      {id: 3, tittle: 'practice js'},
+      {id: 4, tittle: 'practice '},
+      {id: 5, tittle: 'do it'}
     ]
   },
   mutations: {
